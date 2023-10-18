79467d622a63139e80b9c99cb3b0c2c5cdd5372a
Cazpar <casti19@student.sdu.dk>
2023-10-17 19:39:21 -0700

    feat: :sparkles: Finished Exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+    const [start, end] = args;
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
