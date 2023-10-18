a11ff783a656245a6c4089d79e6b195d511f752a
Cazpar <casti19@student.sdu.dk>
2023-10-17 19:48:27 -0700

    feat: :sparkles: Finished exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesInFahrenheit) {
+  let celsius = (degreesInFahrenheit - 32) * 5 / 9;
+  //force to 1 decimal place and number
+  return celsius.toFixed(1) * 1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesInCelsius) {
+  let fahrenheit = degreesInCelsius * 9 / 5 + 32;
+  return fahrenheit.toFixed(1) * 1;
 };
 
 // Do not edit below this line
