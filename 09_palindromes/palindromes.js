6ee2acccd700945f84e6204768cf9b3655647b52
Cazpar <casti19@student.sdu.dk>
2023-10-18 21:00:47 -0700

    feat: :sparkles: Finished exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Remove all non-alphanumeric characters
+    let cleanString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // Reverse the string
+    let reverseString = cleanString.split('').reverse().join('');
+    // Compare the two strings
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
